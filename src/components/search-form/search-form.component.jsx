a3541e78407595a978fc96e2335e25e42c88ac8d
Elmi Rabelo <rabeloelmi@gmail.com>
2020-01-15 22:07:37 -0300

    SearchForm refatorado, agora é component mais limpo, o papel de fazer o MovieRequest e SetError foi transferido para o container component Search

--- src/components/search-form/search-form.component.jsx
+++ src/components/search-form/search-form.component.jsx
@@ -1,30 +1,15 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 
-import { connect } from "react-redux";
-import { bindActionCreators } from "redux";
-import { Creators as MoviesActions } from "../../redux/ducks/movies.ducks";
-import { Creators as ErrorActions } from "../../redux/ducks/error.ducks";
-
-import { convertSearchToGenre } from "../../utils/movieUtils";
-
 import { Container } from "./search-form.styles";
 
-//renderiza o input de busca e é resposavel por fazer o request
-const SearchForm = ({ placeholder, getMovieRequest, genres, setError }) => {
+//renderiza o input de busca - invoca submitHandler, reposanvel por fazer o MoviesRequest
+const SearchForm = ({ placeholder, submitHandler }) => {
   const [inputValue, setInputValue] = useState("");
 
   const handleSubmit = e => {
     e.preventDefault();
-
-    if (!inputValue) {
-      return setError(
-        "É necessário digitar o nome de um filme, ou gênero, jovem padawan."
-      );
-    }
-    //converte o valor de input para um genero e retorna a id desse genero, se for um genero valido.
-    const genreId = convertSearchToGenre(inputValue, genres);
-    getMovieRequest({ inputValue, genreId });
+    submitHandler(inputValue);
     setInputValue("");
   };
   return (
@@ -43,14 +28,7 @@ const SearchForm = ({ placeholder, getMovieRequest, genres, setError }) => {
 
 SearchForm.propTypes = {
   placeholder: PropTypes.string.isRequired,
-  getMovieRequest: PropTypes.func.isRequired
+  submitHandler: PropTypes.func.isRequired
 };
 
-const mapStateToProps = state => ({
-  genres: state.movies.genres
-});
-
-const mapDispatchToProps = dispatch =>
-  bindActionCreators({ ...MoviesActions, ...ErrorActions }, dispatch);
-
-export default connect(mapStateToProps, mapDispatchToProps)(SearchForm);
+export default SearchForm;
--- src/pages/Search/Search.page.jsx
+++ src/pages/Search/Search.page.jsx
@@ -1,15 +1,49 @@
 import React from "react";
-
+import PropTypes from "prop-types";
+//Redux and utils
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { Creators as MoviesActions } from "../../redux/ducks/movies.ducks";
+import { Creators as ErrorActions } from "../../redux/ducks/error.ducks";
+import { convertSearchToGenre } from "../../utils/movieUtils";
+//components and styles
 import SearchForm from "../../components/search-form/search-form.component";
 import MoviesOverview from "../../components/movies-overview/movies-overview.component";
-
 import { Container } from "./Search.styles";
 
-const Search = () => (
-  <Container>
-    <SearchForm placeholder="Busque um filme por nome, ou gênero..." />
-    <MoviesOverview />
-  </Container>
-);
+const Search = ({ getMovieRequest, genres, setError }) => {
+  const submitHandler = inputValue => {
+    if (!inputValue) {
+      return setError(
+        "É necessário digitar o nome de um filme, ou gênero, jovem padawan."
+      );
+    }
+    //converte o valor de input para um genero e retorna a id desse genero, se for um genero valido.
+    const genreId = convertSearchToGenre(inputValue, genres);
+    getMovieRequest({ inputValue, genreId });
+  };
+  return (
+    <Container>
+      <SearchForm
+        placeholder="Busque um filme por nome, ou gênero..."
+        submitHandler={submitHandler}
+      />
+      <MoviesOverview />
+    </Container>
+  );
+};
 
-export default Search;
+Search.propTypes = {
+  getMovieRequest: PropTypes.func.isRequired,
+  genres: PropTypes.array.isRequired,
+  setError: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  genres: state.movies.genres
+});
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators({ ...MoviesActions, ...ErrorActions }, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Search);
